58b8f27c8e129e8c5a238b47e9bd54eab311d396
Nirakar <goswaminirakr65@gmail.com>
2023-01-08 06:49:39 +0530

    new cube

--- src/components/visualmemory/visualmemory.js
+++ src/components/visualmemory/visualmemory.js
@@ -10,10 +10,18 @@ const Visualmemory = () => {
     const [mianarry, setMainarry] = useState([])
     const [Start, setStart] = useState(true)
     const [leve, setLevel] = useState(3)
-    const [levelArry,setLevelArry] = useState()
-    const[ans,setAns] = useState()
-    const [newobj,setNewobj] = useState({})
-     
+    const [levelArry, setLevelArry] = useState([])
+    const [ans, setAns] = useState([])
+    const [newobj, setNewobj] = useState({})
+    const [gamestart, setGAmestart] = useState(false)
+    const [chnce, setchance] = useState(3)
+    const [mianchange, setmincahnce] = useState(3)
+    const [value, setValue] = useState()
+    const [ansarry, setAnsarry] = useState([])
+    const [count, setCount] = useState(1)
+    const [nocube,setnocube] = useState(9)
+const [newwidth,setnewwidth] = useState(370 / 3)
+
     const Noarry = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
@@ -22,23 +30,35 @@ const Visualmemory = () => {
     }
 
     const Nomaker = () => {
-        const MianArry = [...Array(9).keys()]
+
+        const MianArry = [...Array(nocube).keys()]
         const NewArryobj = [];
         MianArry.map((x) => {
             const obj = {
-                key : x ,
-                animate:false,
-                bacg:false
+                key: x,
+                animate: false,
+                bacg: false
             }
             newobj[`${x}`] = obj
+
+
             NewArryobj.push(obj)
         })
-         console.log(NewArryobj)
-       
-        for (let i = 0; i < leve ; i++) {
-            const Rendno = randomIntFromInterval(0, 9)
-                
-            NewArryobj[`${Rendno}`].animate = true
+        console.log(leve)
+
+        for (let i = 0; i < leve; i++) {
+            const Rendno = randomIntFromInterval(0, 8)
+            console.log(Rendno)
+            if (NewArryobj[`${Rendno}`].animate) {
+
+                i--
+            } else {
+
+                setAns(x => [...x, Rendno])
+
+                NewArryobj[`${Rendno}`].animate = true
+            }
+
             // if(mianarry.indexOf(Rendno) !== -1){
             //     i--
             // }else{
@@ -49,71 +69,155 @@ const Visualmemory = () => {
 
             // }
         }
-        console.log(newobj)
-        console.log(mianarry)
+
+
         setLevelArry(NewArryobj)
-     
+
     }
 
-    
+
     // Object.entries(newobj).forEach(([key, value]) => {
     //     console.log(`${key}`)
     // });
-const Cubeclikck = (x) => {
-console.log(x)
-if(x){
-    let obj = newobj[`${x}`]
-    obj.bacg = true
-    console.log(newobj[`${x}`])
-    setNewobj(prevState => (
-        {...newobj ,
-            ...prevState[`${x}`].bacg = true
+    const Cubeclikck = (x) => {
+
+        const ansarry = []
+        console.log(count)
+
+        console.log(x)
+        if (!gamestart) {
+            setGAmestart(true)
+        }
+        console.log(ans, ans.includes(x))
+        if (ans.includes(x)) {
+            console.log("ankit")
+            console.log(ansarry.length)
+            console.log(ans.includes(x))
+            let obj = newobj[`${x}`]
+            obj.bacg = true
+            setNewobj(prevState => (
+                {
+                    ...newobj,
+                    ...prevState[`${x}`].bacg = true
+
+                }
+            )
+            )
+            setCount(count + 1)
+            ansarry.push(x)
+          
+
+          console.log(leve,count)
+            if (count == leve) {
+                console.log(count)
+                console.log("cunt3")
+                setGAmestart(false)
+
+                setTimeout(() => {
+                    console.log("500")
+
+                    const MianArry = [...Array(9).keys()]
+                    const NewArryobj = [];
+                    MianArry.map((x) => {
+                        const obj = {
+                            key: x,
+                            animate: false,
+                            bacg: false
+                        }
+                        newobj[`${x}`] = obj
+
+
+                        NewArryobj.push(obj)
+                    })
+                    console.log(NewArryobj)
+                    setLevelArry(NewArryobj)
+
+                }, 500)
+
+                setTimeout(() => {
+
+                    console.log("1000")
+
+
+
+                    setCount(1)
+                        if(leve < nocube){
+
+                        }
+
+                    setLevel((x) => {
+                        return (x + 2)
+                    })
+
+                }, 1000)
+            }
+        } else {
+
+            setValue(x)
+            setTimeout(() => {
+                setValue()
+                setFlashcount()
+                setMainarry([])
+                setLevelArry()
+                setAns([])
+                setCount(0)
+                setGAmestart(false)
+                setchance(chnce - 1)
+                Nomaker()
+            }, 500)
 
         }
-    ))
-    
-}
 
-}
 
-    
+
+    }
+    console.log(leve)
+
     useEffect((x) => {
         Nomaker()
+    
+        // if(){
+           
 
-    }, [])
+          
+        // }
+       
+
+    }, [leve])
 
 
     useEffect(() => {
         !Start && Sowcube()
-    },[newobj])
+    }, [newobj])
 
-console.log(levelArry)
-  
-    const Sowcube = () =>{
-        const  A = Object.keys(newobj)
-       
-      return Object.keys(newobj).map((x,y) =>  
-      {
-        const Item = newobj[`${x}`]
-        console.log(Item)
-   const classs = ` Cube ${Item.animate && "Animate"} `
-   return (<div key={y}  style={{backgroundColor: Item.bacg == true ? "white" : ""}} onClick={() => Cubeclikck(Item.key)}  className={classs} >
-       {Item.key}
-   </div>)
-      })
-           
-       
-        
+
+    const Sowcube = () => {
+        console.log('mmm')
+        console.log(levelArry)
+
+
+        return Object.keys(levelArry).map((x, y) => {
+            const Item = levelArry[`${x}`]
+
+            const classs = ` Cube ${!gamestart && Item.animate && "Animate"}`
+            console.log(classs)
+            return (<div key={y} style={{width : `${newwidth}px` ,  backgroundColor: Item.bacg == true ? "white" : value == Item.key ? "red" : "", opacity: Item.bacg == true ? "1" : value == Item.key ? "1" : "0.15" }} onClick={() => Cubeclikck(Item.key)} className={classs} >
+                {Item.key}
+            </div>)
+        })
+
+
+
     }
-    
-    // !Start && Object.keys(newobj).forEach(x => 
-  
-    // (<div>
-    //         ok
-    //     </div>)
-        
-    ;
-        
+
+        // !Start && Object.keys(newobj).forEach(x => 
+
+        // (<div>
+        //         ok
+        //     </div>)
+
+        ;
+
 
     return (
         <>
